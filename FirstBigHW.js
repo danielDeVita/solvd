cb41f769675d99ec82b2d31c478235237235c541
danielDeVita <danielitodevita@gmail.com>
2023-08-16 10:46:35 -0300

    task 6 done

--- FirstBigHW.js
+++ FirstBigHW.js
@@ -159,7 +159,7 @@ inputElement.addEventListener("input", event => {
 ////////////////////////////////////////////////////////////////////////////
 
 
-/* TASK 5  Implementing Throttle Function */
+/* TASK 5 Implementing Throttle Function */
 
 function throttle(fn, interval) {
     let lastTime = 0;
@@ -186,3 +186,28 @@ window.addEventListener("scroll", throttledScrollHandler);
 ////////////////////////////////////////////////////////////////////////////
 
 
+/* TASK 6 Currying Function Implementation */
+
+function curry(func, arity) {
+    return function curried(...args) {
+        if (args.length >= arity) {
+            return func(...args);
+        } else {
+            return function (...remainingArgs) {
+                return curried(...args, ...remainingArgs);
+            };
+        };
+    };
+};
+
+function multiply(a, b, c) {
+    return a * b * c;
+};
+
+const curriedMultiply = curry(multiply, 3);
+
+const step1 = curriedMultiply(2); // Returns a curried function  
+const step2 = step1(3); // Returns a curried function  
+const result = step2(4); // Returns the final result: 2 * 3 * 4 = 24  
+
+// console.log("Result:", result); // Expected: 24
