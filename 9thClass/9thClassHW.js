d0a269b39277e564c8965247fe4118934f79887d
danielDeVita <danielitodevita@gmail.com>
2023-08-23 09:06:34 -0300

    algorithms done and working, lacking comparisson

--- 9thClass/9thClassHW.js
+++ 9thClass/9thClassHW.js
@@ -4,23 +4,29 @@ const quickSort = array => {
     //break case for recursion
     if (array.length <= 1) return array;
     //pivot element from array done with random method
-    const pivot = array[Math.floor(Math.random() * array.length)];
+    const pivotIndex = Math.floor(Math.random() * array.length);
+    const pivot = array[pivotIndex];
     //minor than array
     const left = [];
     //larger than array
     const right = [];
     //iteration and comparison between array element and pivot element
-    for (let i = 1; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
+        //we dont' do nothing if the element is the same as the pivotIndex
+        if (i === pivotIndex) continue;
+        //if it is lesser than pivot we push to left array
         if (array[i] < pivot) left.push(array[i])
+        //if it is greater than pivot we push to right array
         else right.push(array[i])
     };
     //invoking recursion with every array concatenated until reaching break case
     return quickSort(left).concat(pivot).concat(quickSort(right));
 }
 
-// console.log(quickSort([5, 1, 4, 2, 8]));
-// console.log(quickSort([10, 10, 16, 12]));
-// console.log(quickSort([10, -2, -7, 4]));
+// console.log("QUICK SORT")
+// console.log(quickSort([11, 22, 34, 45, 64, 90]));
+// console.log(quickSort([90, 64, 45, 34, 22, 11]));
+// console.log(quickSort([34, 64, 11, 45, 90, 22]));
 
 
 
@@ -62,14 +68,29 @@ const mergeSort = array => {
     return mergeHelper(mergeSort(left), mergeSort(right))
 }
 
-// console.log(mergeSort([5, 1, 4, 2, 8]));
-// console.log(mergeSort([10, 10, 16, 12]));
-// console.log(mergeSort([10, -2, -7, 4]));
+// console.log("MERGE SORT");
+// console.log(mergeSort([11, 22, 34, 45, 64, 90]));
+// console.log(mergeSort([90, 64, 45, 34, 22, 11]));
+// console.log(mergeSort([34, 64, 11, 45, 90, 22]));
 
 
 
 /* BUBLE SORT */
 
-const bubleSort = array =>{
-    
+const bubleSort = array => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let aux = array[i]
+                array[i] = array[j];
+                array[j] = aux;
+            }
+        }
+    }
+    return array;
 }
+
+// console.log("BUBLE SORT");
+// console.log(bubleSort([11, 22, 34, 45, 64, 90]));
+// console.log(bubleSort([90, 64, 45, 34, 22, 11]));
+// console.log(bubleSort([34, 64, 11, 45, 90, 22]));
