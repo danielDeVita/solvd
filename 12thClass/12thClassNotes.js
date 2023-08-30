46260a110b000212be6ece02632b126896f171ed
danielDeVita <danielitodevita@gmail.com>
2023-08-30 18:10:04 -0300

    12thclass note/links

---
+++ 12thClass/12thClassNotes.js
@@ -0,0 +1,9 @@
+/* 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+https://blog.greenroots.info/javascript-promises-explain-like-i-am-five
+
+https://kevinyckim33.medium.com/what-are-promises-in-javascript-f1a5fc5b34bf
+
+*/
