8bdac44862e424db6f663269adaa2518516f0a43
danielDeVita <danielitodevita@gmail.com>
2023-09-14 09:57:30 -0300

    project managment class

---
+++ ProjectManagment/ProjectManagment.js
@@ -0,0 +1,112 @@
+/* 
+
+Project Managment for Developers:
+
+Waterfall: (serialized, step by step, one after another)
+1) Requirements (agree up front)
+2) Design
+3) Implementation
+4) Testing
+5) Maintnence
+
+****************************
+
+Agile: (cycling)
+1) Requirements 
+2) Plan
+3) Design
+4) Develop
+5) Release
+6) Truck and Monitor
+
+****************************
+
+Scrum: (agile framework)
+1) User stories (product vision)
+2) Product backlog
+3) Planning meeting
+4) Sprint backlog (2-4 weeks), daily standup
+5) Deployment (sprint review/retrospective)
+
+Product Owner = he owns the requirements for the idea and explains
+the requirements for the team
+
+****************************
+
+Kanban:
+1) Backlog
+2) Selected
+3) Develop:
+        a) Ongoing
+        b) Done
+4) Deploy
+
+****************************
+
+Project Roles and responsabilities:
+
+1)Project manager:
+    a.	Manages project from start to finish;
+    b.	Develops plans and schedules;
+    c.	Coordinates with all other teams in the organization;
+    d.	Ensures that work is completed on time and within budget;
+    e.	Helps ensure quality of product through testing and reviews;
+    f.	Provides status reports to management at regular intervals throughout development process;
+    g.	Communicates progress, issues or problems to stakeholders as needed;
+    h.	Makes sure everyone knows what’s going on inside the company about projects they are working on;
+
+2)Team lead:
+    a.	Works closely together with the Project Manager to develop and maintain high level strategy and direction for the entire team;
+    a.	Works closely with Project Manager to develop plan and schedule;
+    b.	Responsible for managing people resources such as developers, testers etc.;
+    c.	In charge of communication between members of the team;
+    d.	Keeps track of tasks assigned by PM;
+    e.	Reports any problem encountered during the course of the project to the project manager;
+    f.	Participate in meetings with client;
+    g.	Make sure everything goes smoothly and efficiently;
+    h.	Ensure that every member of the team has enough knowledge and skills to be successful;
+
+3)Developer:
+    a.	Work independently but collaboratively;
+    b.	Develop code according to specifications provided by the Team Lead;
+    c.	Write unit tests;
+    d.	Review each others' codes before merging them into master branch;
+    e.	Follow coding standards;
+    f.	Document their own work using comments and commit messages;
+
+4)QA:
+    a.	Test software after it's been developed;
+    b.	Perform manual testing;
+    c.	Report bugs found;
+    d.	Fix reported bug(s);
+    e.	Submit fixes back to developer/team leader;
+    f.	Provide feedback to Product Owner regarding changes made;
+
+5)Business analyst:
+    a.	Understand business requirements;
+    b.	Create user stories based on those requirements;
+    c.	Prioritize user story list;
+    d.	Assign priority level (P0-P3) to each user story;
+    e.	Identify dependencies amongst user stories;
+    f.	Estimate effort required to complete a task;
+    g.	Plan out how to implement features;
+
+6)Product owner:
+    a.	Define scope of application;
+    b.	Decide which feature should go first;
+    c.	Manage prioritization of features;
+
+7)Scrum master:
+    a.	Coordinate scrum events;
+    b.	Facilitate daily standup meeting;
+    c.	Resolve conflicts when necessary;
+    d.	Monitor status of sprint;
+    e.	Help teams improve themselves through coaching sessions;
+    f.	Take action if needed;
+
+8)UX-UI:
+    a.	Design mockups;
+    b.	Research UX best practices;
+    c.	Implement designs;
+
+*/
